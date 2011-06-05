2f4900e0bd890610f7a9378d371f34815c822950
Taras <totallster@gmail.com>
2011-06-05 22:02:45 +0300

    Finished first cut of the User model

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Twitter::Application.routes.draw do
-  
-  #get "users/new"
-  
+
   match '/signup',  :to => 'users#new'
   match '/contact', :to => 'pages#contact'
   match '/about',   :to => 'pages#about'
